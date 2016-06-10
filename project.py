bab7aada109dfb5d1c5c268efeb5c82a6f20e2eb
rbaas <oepn01102@gmail.com>
2016-06-10 07:52:00 +0000

    Output op basis van ID en antwoord > wordt geschreven naar text file

--- project.py
+++ project.py
@@ -4,20 +4,30 @@ import sys
 import socket
 from lxml import etree
 
+OUTPUT_FILE = 'output.txt'
+
 # Analyseer de vraag: bepaal het soort vraag
+# Wanneer? Wie? Wat? Welke? Hoeveel? In welk(e)? Hoe --?
 def analyse_question(question):
+  # Geef de vraag door naar de alpino parser, verkrijg de XML
+  # Zoek naar de keywords als Wie, Wanneer, Wat, etc. mbv xpath
   print(question)
-  # Wanneer? Wie? Wat? Welke? Hoeveel?
+  
 
 # Output format: ID \t Answer1 \t Answer2...
 def give_output(ID, answers):
-  #TODO: zorg dat answers een lijst antwoorden kan zijn
-  #TODO: Als er geen antwoorden in zitten, meld dit.
-  #if answers is None:
-  
-  line = str(ID) + '\t' + answers
-  
-  print(line)
+  f = open(OUTPUT_FILE, 'a')
+  # Als answers leeg is, geef aan dat er geen antwoord is gevonden
+  if answers == []:
+    line = 'Geen antwoord gevonden'
+  else:
+    # Er zijn 1 of meer antwoorden: maak van de lijst een string gescheiden door tabs
+    line = '\t'.join(str(answer) for answer in answers)
+    
+  line = str(ID) + '\t' + line
+  # Append de regel aan de output file, gescheiden met een newline
+  f.write(line + '\n')
+  #print(line)
 
 # Input format: ID \t Question
 def find_answer(sentence):
@@ -41,6 +51,8 @@ def alpino_parse(sent, host='zardoz.service.rug.nl', port=42424):
   return xml
 
 def main(argv):
+  # Maak de output.txt file eerst leeg voordat de antwoorden er bij in komen
+  open(OUTPUT_FILE, 'w').close()
   # Hoog ID op met 1 elke iteratie over sys.stdin. Vervang ID door het ID van de vraag indien aanwezig.
   ID = 0
   #TODO: tijdelijk gewoon meteen de vragen txt meegeven voor het gemak
@@ -57,8 +69,9 @@ def main(argv):
       ID = ID + 1
       question = sentence
     
+    answers = ['Answer 1', 2, 'Answer 3']
     #TODO: stuur de vraag door voor analyse, SPARQL
-    give_output(ID, question)
+    give_output(ID, answers)
     #find_answer(sentence)
     
 if __name__ == "__main__":
