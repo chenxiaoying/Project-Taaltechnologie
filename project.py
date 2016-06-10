3c821190f4370158d99300319c0518c646b1ad53
rbaas <oepn01102@gmail.com>
2016-06-10 07:25:27 +0000

    Nettere input, verbeterde vraag ID bepaling

--- project.py
+++ project.py
@@ -8,16 +8,16 @@ from lxml import etree
 def analyse_question(question):
   print(question)
   # Wanneer? Wie? Wat? Welke? Hoeveel?
-  
-  # Indien van de .txt file een vraag gelezen: haal de vraag uit de regel
-  if '\t' in question: 
-    question = question[question.index('\t')+1:]
-  
-  print(question)
-  # Parse de vraag met Alpino
-  #xml = alpino_parse(question)
 
 # Output format: ID \t Answer1 \t Answer2...
+def give_output(ID, answers):
+  #TODO: zorg dat answers een lijst antwoorden kan zijn
+  #TODO: Als er geen antwoorden in zitten, meld dit.
+  #if answers is None:
+  
+  line = str(ID) + '\t' + answers
+  
+  print(line)
 
 # Input format: ID \t Question
 def find_answer(sentence):
@@ -41,9 +41,24 @@ def alpino_parse(sent, host='zardoz.service.rug.nl', port=42424):
   return xml
 
 def main(argv):
-  print("Type een vraag")
+  # Hoog ID op met 1 elke iteratie over sys.stdin. Vervang ID door het ID van de vraag indien aanwezig.
+  ID = 0
+  #TODO: tijdelijk gewoon meteen de vragen txt meegeven voor het gemak
+  sys.stdin = open('questions.txt', 'r', encoding='utf-8')
+  
   for sentence in sys.stdin:
-    analyse_question(sentence)
+    sentence = sentence.rstrip()
+    # Haal het ID en de vraag uit de zin
+    if '\t' in sentence:
+      ID = sentence[0:sentence.index('\t')]
+      question = sentence[sentence.index('\t')+1:]
+    # Geen ID voor de vraag: sentence is de gehele vraag, geef het een ID
+    else:
+      ID = ID + 1
+      question = sentence
+    
+    #TODO: stuur de vraag door voor analyse, SPARQL
+    give_output(ID, question)
     #find_answer(sentence)
     
 if __name__ == "__main__":
